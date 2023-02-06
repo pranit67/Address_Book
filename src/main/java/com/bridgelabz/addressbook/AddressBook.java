a06367adea372bc8b75dc9418afe5af3e1075e83
pranit67 <pranit67@gmail.com>
2023-02-06 18:53:33 +0530

    Added code for Multiple contact

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,36 +1,62 @@
 package com.bridgelabz.addressbook;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class AddressBook {
+public class AddressBook extends Contact {
     Scanner scan = new Scanner(System.in);
-    private Contact contact = new Contact();
+    public Contact createContact(ArrayList list) {
+        Contact contact = new Contact();
 
-    public void createContact() {
         System.out.print("Please Enter The First Name :: ");
-        String firstName = scan.next();
+        firstName = scan.next();
         contact.setFirstName(firstName);
         System.out.print("Please Enter The Last Name :: ");
-        String lastName = scan.next();
+        lastName = scan.next();
         contact.setLastName(lastName);
         System.out.print("Please Enter The Address :: ");
-        String address = scan.next();
+        address = scan.next();
         contact.setAddress(address);
         System.out.print("Please Enter The City :: ");
-        String city = scan.next();
+        city = scan.next();
         contact.setCity(city);
         System.out.print("Please Enter The State :: ");
-        String state = scan.next();
+        state = scan.next();
         contact.setState(state);
         System.out.print("Please Enter The Zip Code :: ");
-        int zipCode = scan.nextInt();
+        zipCode = scan.nextInt();
         contact.setZipCode(zipCode);
         System.out.print("Please Enter The Phone Number :: ");
-        long phoneNumber = scan.nextLong();
+        phoneNumber = scan.nextLong();
         contact.setPhoneNumber(phoneNumber);
         System.out.print("Please Enter The email ID :: ");
-        String emailId = scan.next();
-        contact.setEmailID(emailId);
-        System.out.println(contact);
+        emailID = scan.next();
+        contact.setEmailID(emailID);
+        list.add(contact);
+        return contact;
+    }
+
+    public void addContact(ArrayList list) {
+        AddressBook obj = new AddressBook();
+        String ch;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("DO YOU WANT TO ADD MORE CONTACT");
+            System.out.println("Enter choice 1.to add Contact ");
+            int choice;
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    createContact(list);
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+            }
+            System.out.println("DO YOU WANT TO ADD MORE CONTACT (PRESS y to contune)");
+            ch = scan.next();
+        } while (ch.equals("y") || ch.equals("Y"));
+
+        System.out.println(list);
+
+
     }
 }
--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.addressbook;
-
+import java.util.ArrayList;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        ArrayList list = new ArrayList();
         AddressBook addressBook = new AddressBook();
-        addressBook.createContact();
+        addressBook.createContact(list);
+        addressBook.addContact(list);
+
     }
 }
